2713c181bcd829c1737ef5a1e7bedac840e5897d
xjb <beymy.en@gmail.com>
2017-03-25 19:07:44 +0800

    更新

--- m/js/lib/zepto.js
+++ m/js/lib/zepto.js
@@ -2945,13 +2945,21 @@ window.basedir = '/p';
 
 (function() {
     var flowConfig = [{
-        busi_type: 'SD',
+        busi_type: 'SDZL',
+        acq_inst_id: '48215500',
+        flow: 'sd'
+    }, {
+        busi_type: 'SDJL',
         acq_inst_id: '48215500',
         flow: 'sd'
     }, {
         busi_type: 'DMF',
         acq_inst_id: '48215500',
         flow: 'dmf'
+    }, {
+        busi_type: 'SD',
+        acq_inst_id: '48025500',
+        flow: 'sd'
     }];
 
 
